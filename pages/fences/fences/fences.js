0089c6ad5d306e6ba7fd1894db29fc234ffba566
NiallBunting <niall.bunting@bjss.com>
2020-04-05 01:34:12 +0100

    Add latest fences, will now work out if you win but I may changes this.

--- pages/fences/fences/fences.js
+++ pages/fences/fences/fences.js
@@ -9,19 +9,73 @@ class Fences {
   constructor(boardSize) {
     this.boardLength = boardSize;
     // Create correct boardsive
-    this.boardState.length = boardSize * boardSize;
+    this.boardState.length = (boardSize ** 2) + ((boardSize - 1) ** 2 );
     this.boardState.fill(0);
   }
 
   init() {
   }
 
-  hasWon() {
+   
 
+  hasWon(boardLayout) {
+    //console.log(boardLayout);
+    //console.log((boardLayout >>> 0).toString(2));
+
+    //let ret = (boardLayout << 3) | (boardLayout << 2);
+
+    //let ret = (across << 1) | (across >>> 1) | across;
+
+    //console.log((ret >>> 0).toString(2));
+
+    var i;
+    for (i = this.boardLength; i < this.boardState.length; i++) {
+
+
+      // TODO replace this with a splice and AND
+      if( i % 5 == 0 ) {
+         
+        //check if any become live on the next level
+        //remove others
+        let shouldContinue = 0;
+
+        for (let j = 0; j < this.boardLength; j++) {
+          let currentLocation = i + j;
+          let backLocation = (i - (this.boardLength + (this.boardLength - 1 ))) + j;
+
+          this.boardState[currentLocation] = this.boardState[currentLocation] * this.boardState[backLocation];
+
+          if(this.boardState[currentLocation] > 0){
+            shouldContinue = 1;
+          }
+        }
+
+        if( shouldContinue === 0 ) {
+          return false; 
+        }
+
+        i = i + 2;
+        continue;
+      }
+
+
+
+
+      if ((this.boardState[i] * this.boardState[i - 3]) > 0) {
+        this.boardState[i - 2] = 1;
+      }
+
+      if ((this.boardState[i] * this.boardState[i - 2]) > 0) {
+        this.boardState[i - 3] = 1;
+      }
+
+    }
+
+    return true;
   }
 
   play(player, boardLocation) {
-    if(isValidMove) {
+    if(this.isValidMove(boardLocation)) {
       this.boardState[boardLocation] = player;
     }
   }
@@ -32,4 +86,4 @@ class Fences {
 
 }
 
-var fences = new Fences(5);
+var fences = new Fences(3);
